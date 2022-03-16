03f96afd0b203107ad2b2151e8dd24e81c3fc955
Raquel Rodrigues <raquel.mr.rodrigues@gmail.com>
2022-03-16 14:47:16 -0300

    config bd

--- course/src/main/java/com/raquel/course/resources/UserResource.java
+++ course/src/main/java/com/raquel/course/resources/UserResource.java
@@ -13,7 +13,7 @@ public class UserResource {
 	
 	@GetMapping
 	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Maria", "maria@ggmail.com", "273612718", "1234")
+		User u = new User(1L, "Maria", "maria@ggmail.com", "273612718", "1234");
 		return ResponseEntity.ok().body(u);
 	}
 }
